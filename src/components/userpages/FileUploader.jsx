8893cf0dfbe2bed46d62fe09f1891a8c0d0d16c4
Shadow-009 <manishpurohit560@gmail.com>
2022-04-01 10:52:05 +0530

    file uploader

--- src/components/userpages/FileUploader.jsx
+++ src/components/userpages/FileUploader.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 
 const Fileuploader = () => {
-    const [files,setFiles] = useState('');
+    const [files,setFiles] = useState([{value:''}]);
+    let id = -1;
     
-    const FileObject = (event) => {
-        console.log(event.target.files);
+    const FileUpload = (event) => {console.log(id);
+        
+        setFiles((preval)=>{
+            return[...preval,
+            {value:event.target.value}
+        ]});
     }
-    const errMsg = () => {}
+    const FileSubmit =(e)=>{
+        e.preventDefault();
+        for(let i = 0;i<files.length;i++){
+            console.log(files[i]);
+            
+        }console.log("hi");
+    }
+    //const errMsg = () => {}
     return (
-        <div className='container'>
-            <form>
-            <input type="file" multiple onChange={FileObject} />
-            </form>
-        </div>
+        <div className="container">
+                <div className="row">
+                    <form onSubmit={FileSubmit}>
+                        <h3>Send Files</h3>
+                        <div className="form-group">
+                            <input type="file" multiple id='files' onChange={FileUpload}/>
+                        </div>
+                        
+                        <div className="form-group">
+                            <button className="btn btn-primary" type="submit">Upload</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
     );
 }
 
